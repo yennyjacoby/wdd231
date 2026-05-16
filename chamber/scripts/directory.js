9ef5d9c987004f1712a51102d82e5c4ecd93307a
yennyjacoby <yekaromeroro@gmail.com>
2026-05-16 12:23:42 -0600

    calling the function in js

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,53 @@
+const membersContainer = document.querySelector("#members");
+const gridBtn = document.querySelector("#gridBtn");
+const listBtn = document.querySelector("#listBtn");
+
+let membersData = [];
+
+// getting info from JSON- Fetch
+async function getMembers() {
+    try{
+        const response = await fetch("data/members.json");
+        membersData = await response.json();
+        displayMembers(membersData);
+    }
+
+    catch (error) {
+        console.error ("Error loading members:", error);
+    }
+}
+
+// Displaying the members
+function displayMembers(members) {
+    membersContainer.innerHTML ="";
+
+    members.forEach(member => {
+        const card = document.createElement("div");
+        card.classList.add("member-card");
+
+        card.innerHTML=`
+        <img src="images/${member.image}" alt="${member.name}">
+        <h3>${member.name}</h3>
+        <p>${member.address}</p>
+        <p>${member.phone}</p>
+        <p>${member.website}</p>    
+    `;    
+    membersContainer.appendChild(card);
+    });
+}
+
+// Toggle Grid/List View
+gridBtn.addEventListener("click", () => {
+    membersContainer.classList.add("grid");
+    membersContainer.classList.remove("list");
+});
+
+listBtn.addEventListener("click", () => {
+    membersContainer.classList.add("list");
+    membersContainer.classList.remove("grid");
+});
+
+// Important!! Call the function
+getMembers();
+
+
