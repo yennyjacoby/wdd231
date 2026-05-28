b7f994db26b0ab16d533ed1ea1c741568672b346
yennyjacoby <yekaromeroro@gmail.com>
2026-05-28 10:48:15 -0600

    W04 Learning Activities

---
+++ Week 04/Advance HTML Forms/js/scripts.js
@@ -0,0 +1,22 @@
+// const getString =window.location.search;
+// console.log(getString);
+
+
+const myInfo = new URLSearchParams(window.location.search);
+// console.log(myInfo)
+
+// console.log(myInfo.get('first'));
+// console.log(myInfo.get('last'));
+// console.log(myInfo.get('ordinance'));
+// console.log(myInfo.get('date'));
+// console.log(myInfo.get('location'));
+// console.log(myInfo.get('phone'));
+// console.log(myInfo.get('email'));
+
+document.querySelector('#results').innerHTML = `
+<p>Appointment for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+
+<p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple </p>
+<p>Your phone number: ${myInfo.get('phone')} </p>
+<p>Your email address is: ${myInfo.get('email')}</p> 
+`
