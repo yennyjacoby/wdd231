123208bf7799d9fa55c82ebe756bcf87a32ec030
yennyjacoby <yekaromeroro@gmail.com>
2026-06-06 11:51:17 -0600

    discover

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -11,9 +11,12 @@ function displayItems(places) {
         // build the card element
         const thecard = document.createElement('div')
         const thephoto = document.createElement('img')
+
         // Because I am adding the image site I don't need to add it in .mjs
         thephoto.src = `images/${x.photo_url}`;
         thephoto.alt = x.name
+        thephoto.loading = "lazy"
+
         thecard.appendChild(thephoto)
         
         const thetitle = document.createElement('h2')
@@ -34,3 +37,30 @@ function displayItems(places) {
 }
 
 displayItems(places)
+
+
+
+// LAST VISIT MESSAGE. 
+const message = document.querySelector("#welcomeMessage");
+
+const today= Date.now();
+
+const lastVisit = Number(localStorage.getItem("lastVisit"));
+
+if (!lastVisit) {
+    message.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+
+    const msInDay = 1000 * 60 * 60 *24;
+    const daysBetween = Math.floor((today - lastVisit) / msInDay);
+
+    if (daysBetween < 1) {
+        message.textContent = "Back so soon! Awesome!";
+    }   else if (daysBetween === 1) {
+        message.textContent = "You last visited 1 day ago.";
+    }   else {
+        message.textContent = `You last visited ${daysBetween} days ago.`
+    }
+}
+
+localStorage.setItem("lastVisit", today);
